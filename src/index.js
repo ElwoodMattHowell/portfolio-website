7f7de24995c8dedf14cf8c1ceaa5278c719d80d8
Matt Howell <elwhwl@gmail.com>
2022-05-27 12:55:59 -0400

    added contact form and connected to Google Firebase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-var scroll = true;
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { addDoc, collection } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBfpUscMyvHn-KDln5J55SbGo-WEz5llw8",
+  authDomain: "portfolio-contact-351216.firebaseapp.com",
+  projectId: "portfolio-contact-351216",
+  storageBucket: "portfolio-contact-351216.appspot.com",
+  messagingSenderId: "1025733795924",
+  appId: "1:1025733795924:web:73f39fc16506eca7e00dc1",
+  measurementId: "G-N3V53S9YL5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
 
 //checks for intersectio of spiderman div with "trigger" div.  Spiderman retracts web.
+var scroll = true;
+
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     const spiderman = document.querySelector('.spiderman');
@@ -9,6 +32,7 @@ const observer = new IntersectionObserver(entries => {
     if (entry.isIntersecting && scroll) {
       spiderman.classList.add('spiderman-up');
       spiderman.classList.remove('spiderman-animation');
+      web.classList.remove('web-animation');
       web.classList.add('web-up');
 
       scroll = false;
@@ -17,14 +41,13 @@ const observer = new IntersectionObserver(entries => {
 
     // We're not intersecting, so remove the class!
     spiderman.classList.remove('spiderman-up');
-    spiderman.classList.remove('web-up');
+    web.classList.remove('web-up');
   });
 });
 
 //calls observer
 observer.observe(document.querySelector('.trigger'));
-// observer.observe(document.querySelector('#work-wrapper'));
-// observer.observe(document.querySelector('#contact-wrapper'));
+observer.observe(document.querySelector('#contact-wrapper'));
 
 //scrolls to top on page reload
 window.onbeforeunload = function () {
@@ -42,6 +65,7 @@ function scrollFunction() {
 
     document.querySelector('.page-header').style.background = "none";
   }
+
 }
 
 //nav menu shows when hamburger icon clicked
@@ -49,6 +73,8 @@ function showHamburger() {
   const hamburger = document.querySelector('.hamburger-nav');
   hamburger.style.display === "none" || !hamburger.style.display ? hamburger.style.display = "flex" : hamburger.style.display = "none";
 }
+
+document.querySelector('.menu-btn').addEventListener('click', showHamburger);
 
 window.addEventListener("resize", limitFunc);
 
@@ -58,4 +84,26 @@ function limitFunc() {
   if (width > 710) {
     document.querySelector('.hamburger-nav').style.display = "none";
   }
+}
+
+document.getElementById('submit').addEventListener('click', submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var message = document.getElementById('message').value;
+  console.log(name);
+
+  saveMessage(name, email, message);
+}
+
+// Save messages to firebase
+async function saveMessage(name, email, message) {
+  // Add a new document with a generated id.
+  await addDoc(collection(db, "messages"), {
+    name: name,
+    email: email,
+    message: message
+  });
 }
